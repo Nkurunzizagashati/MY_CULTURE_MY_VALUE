d6b28c92f38e616e3077b765fc86d093fd86e147
Fabrice <f.nkurunziz@alustudent.com>
2025-02-07 13:24:15 +0200

    create the Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<footer className="bg-[#2E4D36] text-white text-center py-4">
+			<p>&copy; 2025 Rwanda AR Heritage. All rights reserved.</p>
+		</footer>
+	);
+};
+
+export default Footer;
