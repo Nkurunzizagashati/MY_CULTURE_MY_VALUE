73c0f707cd5c4f93ca8554d2a0a85f0ba2014204
Fabrice <f.nkurunziz@alustudent.com>
2025-02-07 13:22:02 +0200

    define routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ArtfactsPage from './pages/ArtfactsPage';
+
 function App() {
-	return <></>;
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <HomePage />,
+		},
+		{
+			path: '/artifacts',
+			element: <ArtfactsPage />,
+		},
+		{
+			path: '/artifacts/ar/:id',
+			element: <ArtfactsPage />,
+		},
+	]);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
